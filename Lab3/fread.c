f6db97771f1bbe96538ae994014f5f3b79e3692c
Ishaan Sathaye <ishaan.sathaye@gmail.com>
2022-10-07 09:58:39 -0700

    fixed lab3 files

--- Lab3/fread.c
+++ Lab3/fread.c
@@ -12,13 +12,12 @@
 int main(int argc, char *argv[]) {
 
     char buf[SIZE];
-
     FILE *fp = fopen(argv[1], "r");
     if (fp == NULL) {
         perror(argv[1]);
         exit(1);
     }
-    // Use while loop to read
-    while (fread(buf, 1, SIZE, fp) > 0) {}
+    while ((fread(buf, SIZE, 1, fp)) > 0) {}
 }
+
 // 1, 2, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, and 8192
--- Lab3/read.c
+++ Lab3/read.c
@@ -6,19 +6,16 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-#define SIZE 1
+#define SIZE 8192
 
 int main(int argc, char *argv[]) {
-
     int fd;
     char buf[SIZE];
     if ((fd = open(argv[1], O_RDONLY)) < 0) {
         perror(argv[1]);
     }
-    // Use while loop to read
-    while (read(fd, buf, SIZE) > 0) {}
-
+    while ((read(fd, buf, SIZE)) > 0) {}
+    return 0;
 }
-
 
 // 1, 2, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, and 8192
